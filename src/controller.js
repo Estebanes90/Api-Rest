aa3744cad566e6db9a83b5b2284e3ddc056863e1
Estebanes90 <estebanesco90@gmail.com>
2023-11-01 13:38:19 -0300

    se agrega la funcion de agregar libro

--- src/controller.js
+++ src/controller.js
@@ -7,6 +7,12 @@ class LibrosController{
         res.json(result);
     }
 
+    async add (req, res){
+	    const libro = req.body;
+	    const [result] = await pool.query ('INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?,?,?,?)', [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+	    res.json({"Id insertado": result.insertId});
+    }
+
     async getOne(req, res){
         const id = req.body.id;
         const [result] = await pool.query('SELECT * FROM libros WHERE id = ?', [id]);
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,5 @@ import {libros} from './controller.js';
 export const router = Router()
 
 router.get ('/libros', libros.getAll);
+router.post ('/libro', libros.add);
 router.get ('/libros', libros.getOne);
