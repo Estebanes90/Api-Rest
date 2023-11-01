1fd9ce7aa26904415411a3553505a529f6761f5f
Estebanes90 <estebanesco90@gmail.com>
2023-11-01 14:32:34 -0300

    se agrega manejo de errores con (try catch)

--- src/controller.js
+++ src/controller.js
@@ -8,21 +8,33 @@ class LibrosController{
     }
 
     async add (req, res){
-	    const libro = req.body;
-	    const [result] = await pool.query ('INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?,?,?,?)', [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
-	    res.json({"Id insertado": result.insertId});
+	        const libro = req.body;
+        try {
+	        const [result] = await pool.query ('INSERT INTO libros(nombre, autor, categoria, año_publicacion, ISBN) VALUES (?,?,?,?)', [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+	        res.json({"Id insertado": result.insertId});
+        } catch(error) {
+            console.log('Error al añadir libro:', error);
+        }
     }
 
     async update (req, res){
         const libro = req.body;
-        const [result] = await pool.query('UPDATE libros SET nombre=(?), autor(?), categoria(?), año_publicacion(?), ISBN(?) WHERE id=(?)', [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
-        res.json ({"Registros actualizados": result.changedRows});
+        try{
+            const [result] = await pool.query('UPDATE libros SET nombre=(?), autor(?), categoria(?), año_publicacion(?), ISBN(?) WHERE id=(?)', [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
+            res.json ({"Registros actualizados": result.changedRows});
+        } catch(error){
+            console.log ('Error al actualizar libro:', error);
+        } 
     }
 
     async delete (req, res){
         const libro = req.body;
-        const[result] = await pool.query('DELETE FROM libros WHERE id=(?)', [libro.id]);
-        res.json({"Registros eliminados": result.affectedRows});
+        try{
+            const[result] = await pool.query('DELETE FROM libros WHERE id=(?)', [libro.id]);
+            res.json({"Registros eliminados": result.affectedRows});
+        }catch(error){
+            console.log ('Error al eliminar libro:', error);
+        }
     }
     
 
