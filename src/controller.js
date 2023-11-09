710fba6be1fe5092b1c5379d760a4f8bd1518238
Estebanes90 <89225698+Estebanes90@users.noreply.github.com>
2023-11-09 00:59:32 -0300

    Update controller.js
    Se agrega hash para las contraseñas

--- src/controller.js
+++ src/controller.js
@@ -1,4 +1,5 @@
 import {pool} from './database.js';
+const bcrypt = require('bcrypt'); // Importa la librería bcrypt
 
 class UsuariosController{
 
@@ -11,29 +12,42 @@ class UsuariosController{
 	}
     }
 
-    async add (req, res){
-	const usuario = req.body;
-        try {
-	     const [result] = await pool.query ('INSERT INTO usuarios(nombre, apellido, usuario, email, pass, perfil_id, id_bandera) VALUES (?,?,?,?,?,?,?)', [usuario.nombre, usuario.apellido, usuario.usuario, usuario.email, usuario.pass, usuario.perfil_di, usuario.id_bandera]);
-	     res.json({"Usuario insertado": result.insertId, "message": "El usuario se ha agregado con éxito"});
-        } catch(error) {
-             res.status(500).json({"Error": "Ha ocurrido un error al agregar el usuario"});
-        }
-    }
+    async add(req, res) {
+    const usuario = req.body;
+    try {
+        // Hashear la contraseña
+        const hashedPassword = await bcrypt.hash(usuario.pass, 10); // 10 es el número de rondas de salting
 
-    async update (req, res){
-        const usuario = req.body;
-        try {
-              const [result] = await pool.query('UPDATE usuarios SET nombre=(?), apellido=(?), usuario=(?), email=(?), pass=(?), perfil_id=(?), id_bandera=(?) WHERE usuario=(?)', [usuario.nombre, usuario.apellido, usuario.usuario, usuario.email, usuario.pass, usuario.perfil_di, usuario.id_bandera]);
-              if (result.affectedRows > 0){
-		 res.json ({"message": "El usuario ${usuario.usuario} se ha actualizado con éxito"});
-	      }else{
-		 res.status(404).json({"Error": "No se ha encontrado el usuario ${usuario.usuario}"});
-	    }
-        } catch(error){
-             res.status(500).json({"Error": "Ha ocurrido un error al intentar actualizar el usuario"});
-        } 
+        // Insertar el usuario en la base de datos con la contraseña hasheada
+        const [result] = await pool.query('INSERT INTO usuarios(nombre, apellido, usuario, email, pass, perfil_id, id_bandera) VALUES (?,?,?,?,?,?,?)', [usuario.nombre, usuario.apellido, usuario.usuario, usuario.email, hashedPassword, usuario.perfil_id, usuario.id_bandera]);
+
+        res.json({ "Usuario insertado": result.insertId, "message": "El usuario se ha agregado con éxito" });
+    } catch (error) {
+        res.status(500).json({ "Error": "Ha ocurrido un error al agregar el usuario" });
     }
+}
+
+
+async update(req, res) {
+    const usuario = req.body;
+    try {
+        // Primero verificamos si usuario.pass se proporciona en la solicitud.
+	// Hashear la contraseña si se proporciona en la solicitud
+        if (usuario.pass) {
+            const hashedPassword = await bcrypt.hash(usuario.pass, 10);
+            usuario.pass = hashedPassword; // Actualiza la contraseña hasheada en el objeto usuario
+        }
+        // Realizar la actualización en la base de datos
+        const [result] = await pool.query('UPDATE usuarios SET nombre=?, apellido=?, usuario=?, email=?, pass=?, perfil_id=?, id_bandera=? WHERE usuario=?', [usuario.nombre, usuario.apellido, usuario.usuario, usuario.email, usuario.pass, usuario.perfil_id, usuario.id_bandera, usuario.usuario]);
+        if (result.affectedRows > 0) {
+            res.json({ "message": `El usuario ${usuario.usuario} se ha actualizado con éxito` });
+        } else {
+            res.status(404).json({ "Error": `No se ha encontrado el usuario ${usuario.usuario}` });
+        }
+    } catch (error) {
+        res.status(500).json({ "Error": "Ha ocurrido un error al intentar actualizar el usuario" });
+    }
+}
 
     async deleteLogicoEmail (req, res){
         const usuario = req.body;
